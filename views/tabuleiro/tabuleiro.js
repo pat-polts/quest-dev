c1ffc0697695cac05e270fe58b97e332663734a9
manoel Silva Motoso <manoel.s.motoso@hotmail.com>
2016-09-09 18:17:18 -0300

    templates das telas ,tabuleiro, e questios

---
+++ views/tabuleiro/tabuleiro.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('questAstrazeneca.questions', [])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/tabuleiro', {
+            templateUrl: 'tabuleiro/tabuleiro.html',
+            controller: 'TabuleiroCtrl'
+        });
+    }])
+    .controller('TabuleiroCtrl', function($scope,$cookieStore,Question) {
+        $scope.user = $cookieStore.get('user');
+        if($cookieStore.get('progresso')) {
+            for(var i =1; i <= $cookieStore.get('progresso'); i++) {
+                var casa = document.getElementById(i);
+                casa.style.backgroundColor = '#bd9c31';
+                casa.disabled = true;
+            }
+        }
+        if ( !$cookieStore.get('question[1]')) {
+            Question.query(function (questions) {
+                    console.log("GET questions")
+                    questions.forEach(function (question, index) {
+                        $cookieStore.put('question[' + (index + 1) + ']', question);
+                    })
+                },
+                function (errors) {
+                    console.log(errors);
+                });
+        }
+
+        $scope.openQuiz = function (questionId) {
+
+            if($cookieStore.get('progresso') ) {
+                var posicao = $cookieStore.get('progresso');
+
+                if(posicao==(questionId-1)){
+                    location.href="#!/question/"+questionId;
+                }else{
+                    alert("Voce deve completar a casa  "+(++posicao));
+
+                }
+            }else{
+
+                location.href="#!/question/"+questionId;
+            }
+        }
+
+    });
